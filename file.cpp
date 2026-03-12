6fefe467f58b3d8c7588830283fd9a512b7e9bcd
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-12 10:50:51 +0700

    menambahkan elemen array

--- file.cpp
+++ file.cpp
@@ -49,3 +49,23 @@ void insertionsort(){ // procedure insertion sort
         }
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array";
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+
+    cout << endl;
+}
+
+int main(){
+    input();
+    insertionsort();
+    display();
+    system("pause");
+}
