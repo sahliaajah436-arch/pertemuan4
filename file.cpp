8b593bd46b76ae0828a89c258b8a3ad302efda7e
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-12 10:39:12 +0700

    menambahkan prosedur input

--- file.cpp
+++ file.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int arr[20]; // membuat array dengan panjang data 20
 int n;       // membuat variable inputan n
+
+void input(){ // procedure input
+    while(true){
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if(n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+}
