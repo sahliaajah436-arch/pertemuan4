d0d347b223352d2b2dc87593953c81a6275dd68d
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-12 10:49:54 +0700

    menambahkan step

--- file.cpp
+++ file.cpp
@@ -31,4 +31,21 @@ void input(){ // procedure input
 void insertionsort(){ // procedure insertion sort
     int temp;
     int j;
+
+    for(int i = 0; i < n; i++){
+        temp = arr[i];
+        j = i - 1;
+
+        while(j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+
+        cout << "\nStep " << i << " : ";
+        for(int k = 0; k < n; k++){
+            cout << arr[k] << " ";
+        }
+    }
 }
